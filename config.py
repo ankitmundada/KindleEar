80572785e622bee4c16fb4f35ee0b942d975a782
Ankit Mundada <aomundada@gmail.com>
2019-02-27 15:32:46 +0530

    Change timezone

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ KindleEar配置文件，请务必修改开始两个配置（如果使用uploader
 SRC_EMAIL = "aomundada@gmail.com"  #Your gmail account for sending mail to Kindle
 DOMAIN = "http://readerfeeder.appspot.com/" #Your domain of app
 
-TIMEZONE = 8  #Default timezone, you can modify it in webpage after deployed
+TIMEZONE = 5  #Default timezone, you can modify it in webpage after deployed
 
 DEFAULT_MASTHEAD = "mh_default.gif" #default masthead
 DEFAULT_COVER = "cv_default.jpg" #default cover, leave it empty will not add cover to book
