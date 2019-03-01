adfd8d1d5d985644adb59bb03f75b71583047d09
Ankit Mundada <aomundada@gmail.com>
2019-03-01 17:47:05 +0530

    Added more sources

--- books/Readings.py
+++ books/Readings.py
@@ -12,24 +12,30 @@ class Readings(BaseFeedBook):
     language              = 'en'
     feed_encoding         = "utf-8"
     page_encoding         = "utf-8"
-    max_articles_per_feed = 5
+    max_articles_per_feed = 3
     coverfile             = "cv_the_daily_journal.jpg"
     oldest_article        = 1
     
     remove_classes = ['ec-messages',]
     feeds = [
         ('Editorials', 'https://www.thehindu.com/opinion/feeder/default.rss'),
+        ('Editorials', 'https://www.tribuneindia.com/rss/feed.aspx?cat_id=187&mid=70', True),
+        ('Editorials', 'https://www.tribuneindia.com/rss/feed.aspx?cat_id=34&mid=70', True),
         ('Editorials', 'https://indianexpress.com/section/opinion/editorials/feed/'),
-        ('Editorials', 'http://rss.jagran.com/rss/editorial/nazariya.xml'),
         ('Editorials', 'https://www.orfonline.org/content-type/commentary/feed/'),
+        ('Editorials', 'https://navbharattimes.indiatimes.com/opinions/rssfeedsection/2279782.cms'),
         ('Biz', 'https://inc42.com/features/feed'),
         ('Biz', 'https://yourstory.com/category/ys-in-depth/feed'),
-        ('Biz', 'http://feeds.hbr.org/harvardbusiness'),
+        ('Biz', 'https://feeds.feedburner.com/HarvardBusiness'),
         ('AI', 'https://blog.openai.com/rss/'),
-        ('AI', 'http://aiweekly.co/issues.rss'),
         ('AI', 'https://distill.pub/rss.xml'),
         ('AI', 'https://www.aitrends.com/category/features/feed/'),
         ('AI', 'https://blogs.nvidia.com/blog/category/deep-learning/feed/'),
+        ('AI', 'http://feeds.feedburner.com/blogspot/gJZg', True),
+        ('AI', 'https://deepmind.com/blog/feed/basic/'),
+        ('Curious?', 'http://nautil.us/rss/all'),
         ('From Pocket', 'https://getpocket.com/users/*sso1406313901115562/feed/unread'),
+        # ('AI', 'http://aiweekly.co/issues.rss'), # AI Weekly (Doesn't work as expected)
+        # ('Editorials', 'http://rss.jagran.com/rss/editorial/nazariya.xml'), # Dianik Jagaran (Not so good)
         ]
     
