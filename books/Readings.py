9b67d1349d54de8962355631f5cae88e384a96a5
Ankit Mundada <aomundada@gmail.com>
2019-03-22 18:56:16 +0530

    Update DailyJournal sources. Add a Weekly Journal.

--- books/Readings.py
+++ books/Readings.py
@@ -4,9 +4,9 @@ from bs4 import BeautifulSoup
 from base import BaseFeedBook, URLOpener, string_of_tag
 
 def getBook():
-    return Readings
+    return DailyReadings
 
-class Readings(BaseFeedBook):
+class DailyReadings(BaseFeedBook):
     title                 = 'The Daily Journal'
     description           = 'Editorials from various sources on various topics'
     language              = 'en'
@@ -23,26 +23,25 @@ class Readings(BaseFeedBook):
         ('Editorials', 'https://www.thehindu.com/opinion/interview/feeder/default.rss'),
         ('Editorials', 'https://www.tribuneindia.com/rss/feed.aspx?cat_id=187&mid=70', True),
         ('Editorials', 'https://www.tribuneindia.com/rss/feed.aspx?cat_id=34&mid=70', True),
-        ('Editorials', 'https://indianexpress.com/section/opinion/editorials/feed/'),
         ('Biz', 'https://yourstory.com/category/ys-in-depth/feed'),
         ('Biz', 'http://feeds.feedburner.com/hbr/KsRB'),
         ('AI', 'https://blog.openai.com/rss/'),
         ('AI', 'https://distill.pub/rss.xml'),
         ('AI', 'https://www.aitrends.com/category/features/feed/'),
-        ('AI', 'https://blogs.nvidia.com/blog/category/deep-learning/feed/'),
         ('AI', 'http://feeds.feedburner.com/blogspot/gJZg', True),
         ('AI', 'https://deepmind.com/blog/feed/basic/'),
         ('AI', 'https://towardsdatascience.com/feed/tagged/deep-learning'),
         ('AI', 'https://blog.bigml.com/feed/'),
-        ('AI', 'https://eng.uber.com/tag/machine-learning/feed/'),
-        ('AI', 'https://chatbotslife.com/feed'),
         ('AI', 'https://www.sciencedaily.com/rss/computers_math/artificial_intelligence.xml'),
-        ('Curious?', 'http://nautil.us/rss/all'),
         ('Bookmarked', 'https://www.instapaper.com/rss/6974367/jcrQql6U6UgcuGZVa6uXVs2OVYU'),
         # ('Biz', 'https://inc42.com/features/feed'),
         # ('Biz', 'https://inc42.com/startups/feed'),
         # ('Biz', 'https://yourstory.com/category/funding-investments/feed'), # infrequent
+        # ('AI', 'https://blogs.nvidia.com/blog/category/deep-learning/feed/'), # Mostly Promotional
         # ('AI', 'http://aiweekly.co/issues.rss'), # AI Weekly (Doesn't work as expected)
+        # ('AI', 'https://chatbotslife.com/feed'),
+        # ('AI', 'https://eng.uber.com/tag/machine-learning/feed/'),
+        # ('Editorials', 'https://indianexpress.com/section/opinion/editorials/feed/'), # Okayish
         # ('Editorials', 'https://www.orfonline.org/content-type/commentary/feed/'), # Too long
         # ('Editorials', 'http://rss.jagran.com/rss/editorial/nazariya.xml'), # Dianik Jagaran (Not so good)
         # ('Editorials', 'https://navbharattimes.indiatimes.com/opinions/rssfeedsection/2279782.cms'),
---
+++ books/ReadingsWeekly.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from bs4 import BeautifulSoup
+from base import BaseFeedBook, URLOpener, string_of_tag
+
+def getBook():
+    return ReadingsWeekly
+
+class ReadingsWeekly(BaseFeedBook):
+    title                 = 'The Weekly Journal'
+    description           = 'Editorials from various sources on various topics'
+    language              = 'en'
+    feed_encoding         = "utf-8"
+    page_encoding         = "utf-8"
+    max_articles_per_feed = 3
+    coverfile             = "cv_weekly_news.jpg"
+    oldest_article        = 7
+    deliver_days          = ['Friday']
+    
+    remove_classes = ['ec-messages',]
+    feeds = [
+        ('Nautilus', 'http://nautil.us/rss/all'),
+        ]
+    
